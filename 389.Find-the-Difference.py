168c9efca03fa8f0327266e2d8f580e0b1df7e7d
Krishnaprasad <skpkrishna25@gmail.com>
2026-06-22 15:34:14 +0530

    389 added

---
+++ 389.Find-the-Difference.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        count={}
+        for ch in s:
+            if ch in count:
+                count[ch]+=1
+            else:
+                count[ch]=1
+        for ch in t:
+            if ch not in count or count[ch]==0:
+                return ch
+            count[ch]-=1
