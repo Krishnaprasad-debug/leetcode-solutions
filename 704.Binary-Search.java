bee169524d46d87461b45781e2c79c0a6743f177
Krishnaprasad <skpkrishna25@gmail.com>
2026-06-23 08:59:36 +0530

    leetcode solutions file added

---
+++ 704.Binary-Search.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            int m=left+(right-left)/2;
+            if(target<nums[m]){
+                right=m-1;
+            }else if(target>nums[m]){
+                left=m+1;
+            }else{
+                return m;
+            }
+        }
+        return -1;
+    }
+}
